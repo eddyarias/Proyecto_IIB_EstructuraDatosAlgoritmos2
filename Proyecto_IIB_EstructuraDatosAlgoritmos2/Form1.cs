3595a7690612c4f8da2e139dbd406013dd08c2af
EDDY SANTIAGO ARIAS SARANGO <eddy.arias@epn.edu.ec>
2023-08-24 23:02:09 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_IIB_EstructuraDatosAlgoritmos2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_IIB_EstructuraDatosAlgoritmos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
