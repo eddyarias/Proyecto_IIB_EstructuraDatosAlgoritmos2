6743e95f06e33b0e4b2e14928542634313572692
jonathanpoaquiza <jonathan.poaquiza@epn.edu.ec>
2023-08-26 18:49:41 -0500

    boton

--- Proyecto_IIB_EstructuraDatosAlgoritmos2/Form1.Designer.cs
+++ Proyecto_IIB_EstructuraDatosAlgoritmos2/Form1.Designer.cs
@@ -34,6 +34,8 @@
             lbResultado = new Label();
             lbTarifas = new Label();
             lbRutasOptimas = new Label();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // btnCalcular
@@ -48,7 +50,7 @@
             // 
             // txtOrigen
             // 
-            txtOrigen.Location = new Point(144, 15);
+            txtOrigen.Location = new Point(144, 24);
             txtOrigen.Name = "txtOrigen";
             txtOrigen.Size = new Size(125, 27);
             txtOrigen.TabIndex = 1;
@@ -87,11 +89,31 @@
             lbRutasOptimas.TabIndex = 5;
             lbRutasOptimas.Text = "label1";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(59, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Origen:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Destino:";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(lbRutasOptimas);
             Controls.Add(lbTarifas);
             Controls.Add(lbResultado);
@@ -112,5 +134,7 @@
         private Label lbResultado;
         private Label lbTarifas;
         private Label lbRutasOptimas;
+        private Label label1;
+        private Label label2;
     }
 }
