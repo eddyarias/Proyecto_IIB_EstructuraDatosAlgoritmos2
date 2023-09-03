cf06ce97b52650506fc36e7449078eaf3e27c145
JAVIER ALEXANDER ARIAS SANTILLAN <javier.arias@epn.edu.ec>
2023-09-03 00:13:16 -0500

    topDown

--- Proyecto_IIB_EstructuraDatosAlgoritmos2/ViajePorRio.cs
+++ Proyecto_IIB_EstructuraDatosAlgoritmos2/ViajePorRio.cs
@@ -1,10 +1,8 @@
-﻿namespace Proyecto_IIB_EstructuraDatosAlgoritmos2
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proyecto_IIB_EstructuraDatosAlgoritmos2
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     public class ViajePorRio
     {
         public int[,] tarifas; // Matriz triangular superior de tarifas
@@ -17,23 +15,23 @@
             n = 10; // Número de embarcaderos
             tarifas = new int[,]
             {
-            { 0, 10, 15, 20, 25, 30, 35, 40, 45, 50 },
-            { 0,  0, 12, 18, 24, 28, 33, 38, 43, 48 },
-            { 0,  0,  0,  2, 15, 21, 26, 31, 36, 41 },
-            { 0,  0,  0,  0, 10, 16, 22, 27, 32, 37 },
-            { 0,  0,  0,  0,  0,  8, 14, 19, 24, 29 },
-            { 0,  0,  0,  0,  0,  0, 11, 17, 22, 27 },
-            { 0,  0,  0,  0,  0,  0,  0,  9, 15, 20 },
-            { 0,  0,  0,  0,  0,  0,  0,  0,  7, 13 },
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0,  6 },
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }
+                { 0, 10, 15, 20, 25, 30, 35, 40, 45, 50 },
+                { 0,  0, 12, 18, 24, 28, 33, 38, 43, 48 },
+                { 0,  0,  0,  2, 15, 21, 26, 31, 36, 41 },
+                { 0,  0,  0,  0, 10, 16, 22, 27, 32, 37 },
+                { 0,  0,  0,  0,  0,  8, 14, 19, 24, 29 },
+                { 0,  0,  0,  0,  0,  0, 11, 17, 22, 27 },
+                { 0,  0,  0,  0,  0,  0,  0,  9, 15, 20 },
+                { 0,  0,  0,  0,  0,  0,  0,  0,  7, 13 },
+                { 0,  0,  0,  0,  0,  0,  0,  0,  0,  6 },
+                { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }
             };
 
             dp = new int[n, n];
             rutas = new int[n, n];
         }
 
-        //Milton
+        // Método para calcular el costo mínimo utilizando programación dinámica con enfoque "Top-Down"
         public int CalcularCostoMinimoTopDown(int origen, int destino)
         {
             if (origen >= destino)
@@ -44,8 +42,7 @@
                 return dp[origen, destino];
 
             int costoMinimo = int.MaxValue;
-            int siguienteEmbarcadero = -1; //ruta
-
+            int siguienteEmbarcadero = -1; // Ruta
 
             // Recorremos los posibles embarcaderos intermedios
             for (int k = origen + 1; k <= destino; k++)
@@ -54,10 +51,7 @@
                 int costoRestante = CalcularCostoMinimoTopDown(k, destino);
                 int costoTotal = costoViajeDirecto + costoRestante;
 
-                //// Calculamos el costo mínimo tomando el mínimo entre todas las posibilidades de embarcaderos intermedios
-                //costoMinimo = Math.Min(costoMinimo, costoTotal);
-
-                if (costoTotal < costoMinimo) //ruta
+                if (costoTotal < costoMinimo) // Actualizamos el costo mínimo y la siguiente ruta
                 {
                     costoMinimo = costoTotal;
                     siguienteEmbarcadero = k;
@@ -70,11 +64,6 @@
 
             return costoMinimo;
         }
-
-        //javier, programacion dinamica para el viaje mas barato en rio con el enfoque TOP-DOWn
-        //public int CalcularCostoMinimoTopDown(int origen, int destino)
-        //{
-        //}
 
         public List<int> RecuperarRutaOptima(int origen, int destino)
         {
@@ -95,8 +84,8 @@
 
             return ruta;
         }
-
     }
 }
+
 
 
